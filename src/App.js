ad50842557388abecd75ae9201e1e02f1a157aae
An <mictosieunhan@gmail.com>
2023-08-05 23:40:33 +0700

    2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LayOutDetailSneaker from './LayOutDefault/LayOutDetailSneaker/LayOutDetailSneaker';
-import LayOutMenuSneaker from './LayOutDefault/LayOutMenuSneaker/LayOutMenuSneaker';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import LayOutDetailSneaker from './LayOutDefault/LayOutDetailSneaker/LayOutDetailSneaker';
+// import LayOutMenuSneaker from './LayOutDefault/LayOutMenuSneaker/LayOutMenuSneaker';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path='/' element={} /> */}
-        {/* <Route path='/menusneaker' element={<LayOutMenuSneaker />} />
-        <Route path='/detailsneaker' element={<LayOutDetailSneaker />} /> */}
-      </Routes>
-    </BrowserRouter>
+    <>
+    
+    </>
+    // // <BrowserRouter>
+    //   // <Routes>
+    //     {/* <Route path='/' element={} /> */}
+    //     {/* <Route path='/menusneaker' element={<LayOutMenuSneaker />} />
+    //     <Route path='/detailsneaker' element={<LayOutDetailSneaker />} /> */}
+    //   // </Routes>
+    // // </BrowserRouter>
   );
 }
 
