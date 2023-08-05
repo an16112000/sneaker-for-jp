dace0634ffca5422e7c0006e1e5942cf65994c26
An <mictosieunhan@gmail.com>
2023-08-06 01:34:47 +0700

    10

--- src/Layout/DetailSneaker/DetailSneaker.js
+++ src/Layout/DetailSneaker/DetailSneaker.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { getDataFromSnkrDunk } from "../../GetData/getDataFromSnkrDunk";
 
-const DetailSneaker = ({code, size}) => {
+const DetailSneaker = ({ code, size }) => {
     const [data, setData] = useState([])
     useEffect(
         () => {
-            const fetchApi = async() => {
+            const fetchApi = async () => {
                 const response = await getDataFromSnkrDunk();
                 console.log(response)
                 setData(response)
             }
-            console.log(data)
             fetchApi();
         }, [code, size]
     )
+    console.log(data)
     return (
         <>
             Detail Sneaker Page
