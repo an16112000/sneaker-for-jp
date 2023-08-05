45742bc69ef1c3f40a7074ea24b2dffa1af199c1
An <mictosieunhan@gmail.com>
2023-08-06 00:26:02 +0700

    6

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LayOutDetailSneaker from './LayOutDefault/LayOutDetailSneaker/LayOutDetailSneaker';
 import LayOutMenuSneaker from './LayOutDefault/LayOutMenuSneaker/LayOutMenuSneaker';
+import Home from './LayOutDefault/Home/Home';
 
 function App() {
   return (
 
     <BrowserRouter>
       <Routes>
-        {/* <Route path='/' element={} /> */}
+        <Route path='/' element={<Home />} />
         <Route path='/:typeOfProduct/page/:pageNumber' element={<LayOutMenuSneaker />} />
         <Route path='/product/:sneakerId' element={<LayOutDetailSneaker />} />
       </Routes>
